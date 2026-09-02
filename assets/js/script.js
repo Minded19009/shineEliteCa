a72073dcec2015ee8b0a33f380c61f618ae4f1eb
Zain Khan <sk099086@gmail.com>
2026-09-02 06:48:25 -0700

    made the navbar fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -312,3 +312,13 @@ const servicesSwiper = new Swiper('.servicesSwiper', {
         }
      }
 });
+
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 110) {
+        navbar.classList.add('navbar-fixed')
+    } else {
+        navbar.classList.remove('navbar-fixed');
+    }
+});
